2b5b9c06aced943dac883dae7343b38e950978b1
Nikushaana <nikanashvili@gmail.com>
2025-12-16 03:28:20 +0400

    fix: CORS

--- ts-backend/src/main.ts
+++ ts-backend/src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['http://localhost:3000', 'https://tech-service-alpha.vercel.app'],
+    origin: ['http://localhost:3000', 'https://tech-service-ten.vercel.app'],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
     credentials: true,
   });
