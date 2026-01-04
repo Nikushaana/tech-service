0e68be46535957c12d04817616f6eecf7a0fcbcc
Nikushaana <nikanashvili@gmail.com>
2026-01-05 00:54:32 +0400

    fix: make deliveryid required for just fix off site in admin order update

--- ts-backend/src/order/dto/update-admin-order.dto.ts
+++ ts-backend/src/order/dto/update-admin-order.dto.ts
@@ -1,19 +1,18 @@
-import { IsEnum, IsNumber, IsOptional } from 'class-validator';
+import { IsEnum, IsNumber, IsOptional, ValidateIf } from 'class-validator';
 import { OrderStatus } from 'src/common/types/order-status.enum';
 import { OrderType } from 'src/common/types/order-type.enum';
 
 export class UpdateAdminOrderDto {
     @IsOptional()
+    @IsEnum(OrderType)
+    service_type: OrderType;
+
     @IsNumber()
     technicianId: number;
 
-    @IsOptional()
+    @ValidateIf((o) => o.service_type === OrderType.FIX_OFF_SITE)
     @IsNumber()
     deliveryId: number;
-
-    @IsOptional()
-    @IsEnum(OrderType)
-    service_type: OrderType;
 
     @IsOptional()
     @IsEnum(OrderStatus)
